6924467cc2e13dad43764761b4aaccca7634beeb
Chris_tAt <christati1205@gmail.com>
2023-12-11 21:15:36 -0500

    siguiendo con la implementacion de client, ya realice las rutas con react router dom, defini diferentes rutas, ademas coloque una libreeria de react para validar formularios llamada reack hock form, con eso validares todos los formularios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<h1>home page</h1>}/>
+      {/*sirve para login de usuarios */}
+      <Route path='/login' element={<LoginPage/>}/>
+      {/* sirve para registro */}
+      <Route path='/register' element={<RegisterPage/>}/>
+      {/*listar todas la tareas */}
+      <Route path='/tasks' element={<h1>Tasks Page</h1>}/>
+      {/* añadir una tarea */}
+      <Route path='/add-task' element={<h1>new tasks</h1>}/>
+      {/* mostrar datos de una tarea en especifico */}
+      <Route path='/tasks/:id' element={<h1>update page</h1>}/>
+      {/* perfil del usuario */}
+      <Route path='/profile' element={<h1>profile</h1>}/>
+     </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,22 @@
+import useForm from 'react-hook-form';
+
+
+const RegisterPage = () => {
+    
+
+
+  return (
+    <div>
+      <form action=''>
+        <input type='text' name="username"/>
+        <input type='text' name="email"/>
+        <input type='text' name="password"/>
+      </form>
+    </div>
+  )
+}
+
+export default RegisterPage
+
+
+
