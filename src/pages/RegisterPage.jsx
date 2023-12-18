d7a28aac7822fc5a4fcaeac36e98a3f4fc61119e
Chris_tAt <christati1205@gmail.com>
2023-12-17 21:06:43 -0500

    se realizo el registro y se enlazo con el back, practicmante ya esta registrando, solo falta reorganizar algunos componentes, se uso axios y se modifico el cors para que pueda enlazar con el back

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,22 +1,41 @@
-import useForm from 'react-hook-form';
-
+import { useForm } from "react-hook-form";
+import { registerRequest } from "../api/auth";
 
 const RegisterPage = () => {
-    
+  const { register, handleSubmit } = useForm();
 
+  const onSubmit = handleSubmit(async (values) => {
+    const res = await registerRequest(values);
+    console.log(res);
+  });
 
   return (
-    <div>
-      <form action=''>
-        <input type='text' name="username"/>
-        <input type='text' name="email"/>
-        <input type='text' name="password"/>
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          {...register("username", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="username"
+        />
+
+        <input
+          type="text"
+          {...register("email", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="email"
+        />
+
+        <input
+          type="text"
+          {...register("password", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="password"
+        />
+        <button type="submit">Register</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterPage
-
-
-
+export default RegisterPage;
